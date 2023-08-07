6f2bcc4bb9d1c09d34ce89d65185bc7a5e669d3c
Abdullah <abdullah12.hassan7@gmail.com>
2023-08-07 17:01:08 +0500

    responsive

--- mkniazi/components/About.tsx
+++ mkniazi/components/About.tsx
@@ -17,20 +17,59 @@ worked with more than 20 companies as a freelancer on brand identity, package de
 
   return (
     <div className="flex items-center min-h-screen bg-white flex-col pt-10 ">
-      <Avatar className="w-[190px] h-[190px] z-2 mt-[52px] ">
-        <AvatarImage src="/niazi.png" />
-        <AvatarFallback>MK</AvatarFallback>
-      </Avatar>
+      <div className="flex bg-white flex-col  ml-5">
+        <div className="flex items-center  bg-white flex-col lg:flex-row ml-5">
+          {/* for desktop display */}
+          <Avatar className="w-[190px] h-[190px] z-2 mt-[22px] lg: ml-[50px] hidden lg:block">
+            <AvatarImage src="/niazi.png" />
+            <AvatarFallback>MK</AvatarFallback>
+          </Avatar>
 
-      <Avatar className="w-[300px] h-[300px] z-0 absolute">
-        <AvatarImage src="/ellipse-3.png" />
-        <AvatarFallback>MK</AvatarFallback>
-      </Avatar>
+          <Avatar className="w-[300px] h-[300px] z-0 absolute hidden lg:block">
+            <AvatarImage src="/ellipse-3.png" />
+            <AvatarFallback>MK</AvatarFallback>
+          </Avatar>
 
-      <h3 className="mt-[12%] px-5 text-[16px] text-[#3A3A3A]">{descriptions[0]}</h3>
-      <hr className="w-[92%] mt-[4%] border-[1px] " style={{ color: "black" }} />
+          {/* for mobile */}
 
-      <Accordion type="single" collapsible className="w-[92%] ">
+          <Avatar className="w-[220px] h-[220px] z-2 mt-[46px] lg: ml-[16px] lg:hidden">
+            <AvatarImage src="/niazi.png" />
+            <AvatarFallback>MK</AvatarFallback>
+          </Avatar>
+
+          <Avatar className="w-[310px] h-[310px] z-0 absolute lg:hidden">
+            <AvatarImage src="/ellipse-3.png" />
+            <AvatarFallback>MK</AvatarFallback>
+          </Avatar>
+
+          <h3 className="mt-[12%] px-5 text-[16px] text-[#3A3A3A] lg:hidden font-semibold ">{descriptions[0]}</h3>
+          <h3 className="pb-[10%] pt-[10%] px-5 text-[16px] text-[#3A3A3A] w-[50%] ml-[10%] font-semibold hidden  lg:block">
+            {descriptions[0]}
+          </h3>
+        </div>
+        <div className="hidden lg:flex flex-row ml-[3%] ">
+          <div className="w-[24%] text-[#3A3A3A] mx-[1%]">
+            <h3 className="font-[700] text-sm pb-3">Identity Design</h3>
+            <p className="text-xs font-[500]">{descriptions[1]}</p>
+          </div>
+          <div className="w-[24%] text-[#3A3A3A] mx-[1%] mb-[7%]">
+            <h3 className="font-[700] text-sm pb-3">Pakaging Design</h3>
+            <p className="text-xs font-[500]">{descriptions[1]}</p>
+          </div>
+          <div className="w-[24%] text-[#3A3A3A] mx-[1%] mb-[7%]">
+            <h3 className="font-[700] text-sm pb-3">Web Design</h3>
+            <p className="text-xs font-[500]">{descriptions[1]}</p>
+          </div>
+          <div className="w-[24%] text-[#3A3A3A] ml-[1%]  ">
+          <Button className="hidden rounded-full w-[150px] h-[36px] text-[10px] lg:block mt-[40%] bg-[#FFFFFF] text-black border ml-7 ">SCROLL FOR MORE ▼ </Button>
+          </div>
+         
+        </div>
+      </div>
+
+      <hr className="w-[92%] mt-[4%] border-[1px] lg:hidden " style={{ color: "black" }} />
+
+      <Accordion type="single" collapsible className="w-[92%] lg:hidden">
         <AccordionItem className="border-b-2" value="item-1">
           <AccordionTrigger>Identity Design</AccordionTrigger>
           <AccordionContent>{descriptions[1]}</AccordionContent>
--- mkniazi/components/Portfolio.tsx
+++ mkniazi/components/Portfolio.tsx
@@ -5,29 +5,46 @@ import { AspectRatio } from "@/components/ui/aspect-ratio";
 const Portfolio: React.FC<{}> = () => {
   return (
     <div className="w-[100%] text-white">
-      {/* <AspectRatio className="z-0" ratio={16 / 9}> */}
+      <div className="w-[100%] text-white lg:hidden">
+        {/* <AspectRatio className="z-0" ratio={16 / 9}> */}
         <img src="/work-card-2.png" alt="Photo by Drew Beamer" className=" object-cover " width="100%" />
-      {/* </AspectRatio> */}
+        {/* </AspectRatio> */}
 
-      <div className="w-[100] h-[160px] bg-[#3A3A3A] opacity-50 z-20 mb-10 -mt-[43%] xs:-mt-[30%] sm:-mt-[25%]" >
-        <div className="flex w-[100%] p-3">
-  
-          <h3 className="">01-10</h3>
-          <img className="ml-[79%] h-4 mt-3" src="/arrow-2.png" alt="circle design" />
+        <div className="w-[100] h-[160px] bg-[#3A3A3A] opacity-50 z-20 mb-10 -mt-[43%] xs:-mt-[30%] sm:-mt-[25%]">
+          <div className="flex w-[100%] p-3">
+            <h3 className="">01-10</h3>
+            <img className="ml-[79%] h-4 mt-3" src="/arrow-2.png" alt="circle design" />
+          </div>
+
+          <h2 className="ml-3 text-xl"> Crazy</h2>
+
+          <div className="flex ml-3 mt-3 z-50 text-white ">
+            <h1 className="w-[24%]">TYPE </h1>
+            <h1 className="w-[50%]">DESCRIPTION </h1>
+            <h1 className="w-[24%]">YEAR </h1>
+          </div>
+          <div className="flex ml-3 mt-1 z-50 text-white text-xs sm:text-[13px]">
+            <h1 className="w-[24%]">Identity Web Design </h1>
+            <h1 className="w-[50%]">White-label fintech solution in dubai </h1>
+            <h1 className="w-[24%]">2023 </h1>
+          </div>
+        </div>
+      </div>
+
+
+      <div className="hidden lg:flex bg-black flex-row">
+        <div className="pt-4 w-[50%]  ">
+        <img src="/work-card-details.png" alt="Photo by Drew Beamer" className=" " width="100%" />
         </div>
 
-        <h2 className="ml-3 text-xl"> Crazy</h2>
+        <div className="pt-4 w-[50%] ">
+        <img src="/work-card-2.png" alt="Photo by Drew Beamer" className="  " width="99.93%" />
+        </div>
 
-        <div className="flex ml-3 mt-3 z-50 text-white ">
-          <h1 className="w-[24%]">TYPE </h1>
-          <h1 className="w-[50%]">DESCRIPTION </h1>
-          <h1 className="w-[24%]">YEAR </h1>
-        </div>
-        <div className="flex ml-3 mt-1 z-50 text-white text-xs sm:text-[13px]">
-          <h1 className="w-[24%]">Identity Web Design </h1>
-          <h1 className="w-[50%]">White-label fintech solution in dubai </h1>
-          <h1 className="w-[24%]">2023 </h1>
-        </div>
+        <img src="/arrLeft.png" alt="Photo by Drew Beamer" className="absolute mt-[30%] left-[2%]" width="50px" />
+        <img src="/arrRight.png" alt="Photo by Drew Beamer" className="absolute mt-[30%] right-[2%]" width="50px" />
+     
+
       </div>
     </div>
   );
