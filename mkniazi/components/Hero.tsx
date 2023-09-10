d95c32fb32197da5b1161c8e427477706f331c79
Abdullah <abdullah12.hassan7@gmail.com>
2023-09-11 04:44:28 +0500

    centered

--- mkniazi/components/Hero.tsx
+++ mkniazi/components/Hero.tsx
@@ -16,32 +16,32 @@ const Hero: React.FC<{}> = () => {
         </h4>
         <Button className="rounded-full w-[120px] h-[38px] text-[10px] hidden lg:block ml-[10%] ">Let’s talk</Button>
       </div>
-      <div className=" items-center justify-center lg:flex 2xl:">
+      <div className=" items-center justify-center lg:flex 2xl: h-[64rem]  ">
       <Image
-        className="mt-[9%] h-[50%] lg:ml-[16%] "
+        className=" lg: mr-[5rem]"
         width={450}
         height={450}
         src="/ellipse-3.png"
         alt="circle design"
       />
 {/* settings */}
-      <div className=" pl-3 sm:self-center mt-[4%] ml-5 lg:ml-[10%] 2xl:mt-[5%]">
-        <h3 className="text-[24px] leading-7 sm:text-[30px] leading-9 lg:mt-16">
+      <div className="  lg: 2xl: w-[16rem] ml-[5rem]  ">
+        <h3 className="text-[24px] leading-7 sm:text-[30px] leading-9 ">
           Brand Identity, <br /> Packaging & Web
         </h3>
 
-        <p className="text-[16px] text-[#3A3A3A] mt-2 leading-5 sm:text-[18px] w-[50%] lg:w-[50%]">
+        <p className="text-[16px] text-[#3A3A3A] mt-2 leading-5 sm:text-[18px]">
           Helping brands to stand out in the digital era. Together we will set the new status quo.
         </p>
         <Button className="hidden rounded-full w-[170px] h-[38px] text-[12px] lg:block mt-4 bg-[#ECECEC] text-black border hover:text-white">SCROLL FOR MORE ▼ </Button>
       </div>
       </div>
 
-      <div className="flex mt-4 ml-3 w-[92%] sm: self-center ">
+      <div className="flex mt-4 ml-3 w-[92%] sm: self-center pb-5 ">
         <Button className="rounded-full  w-[120px] h-[33px] text-[10px] hover:text-white lg:hidden">Let’s talk</Button>
-        <div className="w-[100%] flex justify-end">
-          <p className="text-[10px] mt-3 lg:absolute bottom-10 right-[5%] ">@2023</p>
-          <p className="text-[10px] mt-3 lg:absolute bottom-10 left-[5%] hidden lg:block">BEHANCE  &nbsp;&nbsp;    INSTAGRAM  &nbsp;&nbsp;    LINKEDIN </p>
+        <div className="w-[100%] flex justify-end ">
+          <p className="text-[10px] mt-3 lg:absolute bottom-14 right-[5%] ">@2023</p>
+          <p className="text-[10px] mt-3 lg:absolute bottom-14 left-[5%] hidden lg:block">BEHANCE  &nbsp;&nbsp;    INSTAGRAM  &nbsp;&nbsp;    LINKEDIN </p>
         </div>
       </div>
     </div>
